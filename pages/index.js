0b0da59c5bdbc0b5f698f3aceff9d9e2cb32640e
kushalsrinivas <kushal.s.2005@gmail.com>
2023-07-25 17:41:35 +0530

    yeshhsehs

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = (props) => {
 
   async function GETIMGS() {
     const cdn =
-      "https://pqwjzqrgrxyzoxawcriq.supabase.co/storage/v1/object/public/images/";
+      "";
     const { data, error } = await supabase.storage.from("images").list();
     if (data !== null) {
       data.map((img, index) => {
@@ -27,11 +27,7 @@ const Home = (props) => {
     GETIMGS();
   }, []);
 
-  useEffect(() => {
-    console.log("====================================");
-    console.log(data);
-    console.log("====================================");
-  }, [data]);
+
 
   return (
     <>
